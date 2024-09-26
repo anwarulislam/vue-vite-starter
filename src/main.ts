1ca11cf37c36e0bb502e96de49cd71b8c22255ff
Anwarul Islam <anwaarulislaam@gmail.com>
2024-09-26 21:47:27 +0600

    feat: hopp ui added

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,18 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+// Import the library
+import { plugin as HoppUI } from '@hoppscotch/ui'
+
+// Import theme styles for default styling (optional)
+import '@hoppscotch/ui/themes.css'
+
+// Import the styles
+import '@hoppscotch/ui/style.css'
 
 const app = createApp(App)
+// Register the library as a plugin
+app.use(HoppUI)
 
 app.use(router)
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>Welcome to Your Vue.js + Vite App</h1>
+
+    <HoppButtonPrimary label="Button" />
   </main>
 </template>
+
+<script setup lang="ts">
+import { HoppButtonPrimary } from '@hoppscotch/ui'
+</script>
